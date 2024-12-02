2cc3ede38bdbd234f09da34235ab6f9c731d59fa
roslyn-maloney <roselily1604@gmail.com>
2024-12-01 20:10:52 -0500

    3.6.3 is finish

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -10,9 +10,21 @@ export default function UserRoutes(app) {
   const findUserById = (req, res) => { };
 
   const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
     const users = await dao.findAllUsers();
     res.json(users);
-  }
+  };
+
 
   //update user
   const updateUser = (req, res) => {
