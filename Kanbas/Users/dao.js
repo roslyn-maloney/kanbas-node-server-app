69c1fe862940833234b2116a1f9e963645c62837
roslyn-maloney <roselily1604@gmail.com>
2024-12-01 20:13:11 -0500

    init commit

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -23,5 +23,4 @@ export const findUsersByPartialName = (partialName) => {
   const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
   return model.find({
     $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
-  });
-};
+  });};
